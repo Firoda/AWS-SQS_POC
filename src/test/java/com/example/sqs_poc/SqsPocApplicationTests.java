88becca25e88a99fba7cde07bcae57ebe8950f22
Aditya Firoda <adityafiroda@gmail.com>
2021-09-02 14:22:48 +0530

    First Commit

---
+++ src/test/java/com/example/sqs_poc/SqsPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqs_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
